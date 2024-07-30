7b32da3f730deab8de0f0ea5a22076cd7254bb9a
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2024-07-30 04:05:53 +0400

    Initial commit :tada:

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+
+;// CONCATENATED MODULE: ./src/js/window/window.js
+
+
+window.__jr__ = {
+
+};
+
+;// CONCATENATED MODULE: ./src/js/script.js
+
+
+
+
+/******/ })()
+;
