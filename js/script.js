93fa48ba2db94e0b1deeb1bae6db9f9db30df461
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2024-07-30 17:49:13 +0400

    Added loader

--- js/script.js
+++ js/script.js
@@ -8,6 +8,38 @@ window.__jr__ = {
 
 };
 
+;// CONCATENATED MODULE: ./src/js/scripts/scripts/progress.js
+function progress() {
+  /** @type {HTMLSpanElement} */
+  const progressNumbers = document.querySelector(".hero__progress span");
+
+  if (progressNumbers) {
+    updateProgress();
+
+    function updateProgress(number = 0) {
+      setTimeout(() => {
+        progressNumbers.innerText = `${number}`.padStart(2, "0");
+
+        if (number !== 100) {
+          updateProgress(number + 1);
+        }
+      }, Math.random() * 50);
+    }
+  }
+}
+
+
+
+;// CONCATENATED MODULE: ./src/js/scripts/scripts/load.js
+
+
+window.addEventListener("load", () => {
+  progress();
+});
+
+;// CONCATENATED MODULE: ./src/js/scripts/scripts.js
+
+
 ;// CONCATENATED MODULE: ./src/js/script.js
 
 
